b19f2cffc5c7bcb8ffaa6f57bdc78a4b1cbd94cb
Aitor Font <afont@devcenter.es>
2018-11-27 11:32:13 +0100

    Rewrite Board to use two loops to make the squares

--- react-tutorial/src/Board.js
+++ react-tutorial/src/Board.js
@@ -14,21 +14,13 @@ class Board extends Component {
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {[0, 1, 2].map(row =>
+          <div className="board-row">
+            {[0, 1, 2].map(col => 
+              this.renderSquare(col + row + (row * 2))
+            )}
+          </div>
+        )}
       </div>
     );
   }
